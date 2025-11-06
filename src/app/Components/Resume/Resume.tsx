8b44b7a12f219f6e54331bbbacdd51be2184a111
Prem S N <65552127+premsn-1226@users.noreply.github.com>
2025-11-06 08:24:46 +0530

    new resume url update

--- src/app/Components/Resume/Resume.tsx
+++ src/app/Components/Resume/Resume.tsx
@@ -18,7 +18,7 @@ export default function Resume() {
           style={{ marginTop: "-35px" }}
         >
           <a
-            href="https://drive.google.com/file/d/1ewX41jfPvllm9gNYvwBALmQgcZwj-85u/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1M_Jq6lrSohhGwkqv7xM-y19hvwKPWsVO/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
           >
